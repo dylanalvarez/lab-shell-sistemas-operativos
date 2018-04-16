c2c985e25f97be87a868d36c5e6a0c3425168b09
Dylan <dylanalvarez1995@gmail.com>
2018-04-15 21:17:07 -0300

    implement built in commands

--- src/builtin.c
+++ src/builtin.c
@@ -1,4 +1,6 @@
 #include "builtin.h"
+#include "parsing.h"
+#include "freecmd.h"
 
 // returns true if the 'exit' call
 // should be performed
@@ -12,18 +14,33 @@ int exit_shell(char *cmd) {
 // 	$ cd (change to HOME)
 // it has to be executed and then return true
 int cd(char *cmd) {
-
-    // Your code here
-
-    return 0;
+    bool invoked = false;
+    struct execcmd *parsed = (struct execcmd *) parse_line(cmd);
+    if (parsed->argc > 0 && strcmp(parsed->argv[0], "cd") == 0) {
+        if (chdir(parsed->argc == 1 ? getenv("HOME") : parsed->argv[1]) == -1) {
+            perror(SHELL_NAME);
+        } else {
+            char* current_dir = get_current_dir_name();
+            snprintf(prompt, sizeof prompt, "(%s)", current_dir);
+            free(current_dir);
+        }
+        invoked = true;
+    }
+    free_command((struct cmd *) parsed);
+    return invoked;
 }
 
 // returns true if 'pwd' was invoked
 // in the command line
 int pwd(char *cmd) {
-
-    // Your code here
-
-    return 0;
+    bool invoked = false;
+    struct execcmd *parsed = (struct execcmd *) parse_line(cmd);
+    if (parsed->argc > 0 && strcmp(parsed->argv[0], "pwd") == 0) {
+        char* current_dir = get_current_dir_name();
+        printf("%s\n", current_dir);
+        free(current_dir);
+        invoked = true;
+    }
+    free_command((struct cmd *) parsed);
+    return invoked;
 }
-
